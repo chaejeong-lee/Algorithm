5f30cb2b621149f9abc062aff0570a423b005d7b
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2022-11-09 22:03:16 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 158 ms, Memory: 39,208 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/README.md
@@ -0,0 +1,11 @@
+# [D3] [S/W 문제해결 기본] 8일차 - 암호문2 - 1229 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14yIsqAHYCFAYD) 
+
+### 성능 요약
+
+메모리: 39,208 KB, 시간: 158 ms, 코드길이: 1,139 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T=10;
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            LinkedList<Integer> list = new LinkedList<>();
+            int N = sc.nextInt();
+            
+            for(int i = 0 ;i <N ; i++) list.add(sc.nextInt());
+            
+            int K = sc.nextInt();//명령어 개수
+            
+            for(int i = 0 ; i<K ; i++){
+            	String s = sc.next();
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+                if(s.equals("I")){
+                    for(int j = 0 ; j<y ; j++){
+                    	list.add(x, sc.nextInt());
+                        x++;
+                    }
+                }else if(s.equals("D")){
+                    for(int j = 0 ; j<y ; j++){
+                    	list.remove(x);
+                    }
+                }
+            }
+            System.out.print("#"+test_case+" ");
+            for(int i = 0 ; i<10;i++){
+            	System.out.print(list.poll()+" ");
+            }
+            System.out.println();
+		}
+	}
+}
