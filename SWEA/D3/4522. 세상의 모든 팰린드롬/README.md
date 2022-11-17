c945b5bed727006d89b51dd8575ecd89e8341bc8
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2022-11-17 15:44:13 +0900

    [D3] Title: 세상의 모든 팰린드롬, Time: 132 ms, Memory: 27,064 KB -BaekjoonHub

---
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/README.md
@@ -0,0 +1,11 @@
+# [D3] 세상의 모든 팰린드롬 - 4522 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWO6Oao6N4QDFAWw) 
+
+### 성능 요약
+
+메모리: 27,064 KB, 시간: 132 ms, 코드길이: 976 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/세상의 모든 팰린드롬.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String s = sc.next();
+
+			String[] arr = new String[s.length()];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = s.charAt(i) + "";
+			}
+			boolean check = checkPalindrome(arr, s.length());
+			if (check)
+				sb.append("#" + test_case + " Exist\n");
+			else
+				sb.append("#" + test_case + " Not exist\n");
+		}
+		System.out.print(sb);
+	}
+
+	private static boolean checkPalindrome(String[] arr, int num) {// 문자열, 길이
+		int cnt = 0;
+		for (int i = 0; i < num / 2; i++) {
+			if((arr[i].equals("?")) || (arr[num-i-1].equals("?"))) {
+				cnt++;
+				continue;
+			}
+			else if (!arr[i].equals(arr[num - i - 1]))
+				return false;
+			cnt++;
+		}
+		if (cnt == num / 2)
+			return true;
+
+		return false;
+	}
+}
