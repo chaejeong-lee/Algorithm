7f82dbc6d07d34e328ed7f772414ad07606b950f
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2022-11-28 12:46:56 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i=1;
+        while(true){
+            if((6*i)%n == 0){
+                answer = i;
+                break;
+            }
+            i++;
+        }
+        return answer;
+    }
+}
