78473ccedba28f9cf28b4e126c1d3b5dbd0ae90e
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2023-01-17 22:55:36 +0900

    [Bronze V] Title: SciComLove, Time: 204 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			System.out.println("SciComLove");
+		}
+	}
+
+}
