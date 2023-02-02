b82ad296d409792adac20b4411a1088975f998e8
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2023-02-02 23:12:35 +0900

    [Bronze III] Title: 부호, Time: 760 ms, Memory: 120488 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i = 0; i < 3; i++) {
+			BigInteger S = new BigInteger("0");
+			int N = Integer.parseInt(br.readLine());
+			
+			for(int j = 0; j < N; j++) {
+				BigInteger N2 = new BigInteger(br.readLine());
+				S = S.add(N2);
+			}
+			
+			//compareTo(0)이 -1이면 음수, 1이면 양수, 0이면 0
+			if(S.compareTo(BigInteger.ZERO) == -1) {
+				System.out.println("-");
+			}else if(S.compareTo(BigInteger.ZERO) == 1) {
+				System.out.println("+");
+			}else {
+				System.out.println(0);
+			}
+		}
+	}
+
+}
