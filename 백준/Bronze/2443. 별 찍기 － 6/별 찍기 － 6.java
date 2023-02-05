88ee8e0eaa6b8134035d6895b5243ffad9e58a29
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2023-02-05 22:28:56 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 400 ms, Memory: 20988 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i=N;i>0;i--) {
+			for(int j=0;j<N-i;j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j=0;j<2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
