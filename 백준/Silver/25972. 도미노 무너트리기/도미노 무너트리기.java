458c87c94edd5268b602c79e76ed264b433b5086
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2023-02-07 23:57:49 +0900

    [Silver IV] Title: 도미노 무너트리기, Time: 1800 ms, Memory: 149000 KB -BaekjoonHub

--- 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.java
+++ 백준/Silver/25972. 도미노 무너트리기/도미노 무너트리기.java
@@ -11,26 +11,27 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		int[][] arr = new int[N][2];
 		int cnt = 1;
-		for(int i=0;i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());//i번째
-			arr[i][1] = Integer.parseInt(st.nextToken());//길이 : l
+			arr[i][0] = Integer.parseInt(st.nextToken());// i번째
+			arr[i][1] = Integer.parseInt(st.nextToken());// 길이 : l
 		}
-		
+
 		Arrays.sort(arr, Comparator.comparingLong(o1 -> o1[0]));
 
-		int num = arr[0][0];
-		for(int i=0;i<N;i++) {
-			if(num < arr[i][0]) cnt++;
+		int num = arr[0][0]+arr[0][1];
+		for (int i = 0; i < N; i++) {
+			if (num < arr[i][0])
+				cnt++;
 			num = arr[i][0] + arr[i][1];
 		}
 		System.out.println(cnt);
-		
+
 	}
 
 }
