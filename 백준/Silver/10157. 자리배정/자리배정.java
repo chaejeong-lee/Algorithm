f9ca9d5530bc174ea4694e3b59ff6ebc8f24db59
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2023-02-26 23:03:21 +0900

    [Silver IV] Title: 자리배정, Time: 172 ms, Memory: 17044 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.java
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -4,41 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int C, R, K;
+	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int C = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		C = Integer.parseInt(st.nextToken());//가로
+		R = Integer.parseInt(st.nextToken());//세로
+		
+		K = Integer.parseInt(br.readLine());//어떤 관객의 대기 번호
+		
+		if(K>C*R) System.out.println("0");
+		else {
+			boolean[][] visited = new boolean[R][C];
+			int cnt = 0, d = 0;
+			int r=R-1, c =0;
+			while(++cnt != K) {
+				visited[r][c] = true;
+				int nr = r + dir[d][0];
+				int nc = c + dir[d][1];
+				
+				if(nr<0 || nc<0 || nc >=C || nr >= R || visited[nr][nc]) {
+					d = ++d%4;
+					nr = r + dir[d][0];
+					nc = c + dir[d][1];
+				}
+				
+				r = nr;
+				c = nc;
+			}
+			System.out.println((c+1)+" "+(R-r));
+		}
+	}
 
-        int K = Integer.parseInt(br.readLine());
-
-        if (K > C * R) {
-            System.out.println(0);
-            return;
-        }
-
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
-
-        boolean[][] visit = new boolean[R][C];
-
-        int cnt = 0, x = R - 1, y = 0, dir = 0;
-        while (++cnt != K) {
-            visit[x][y] = true;
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if (nx < 0 || ny < 0 || nx >= R || ny >= C || visit[nx][ny]) {
-                dir = ++dir % 4;
-                nx = x + dx[dir];
-                ny = y + dy[dir];
-            }
-
-            x = nx;
-            y = ny;
-        }
-
-        System.out.println((y + 1) + " " + (R - x));
-    }
 }
