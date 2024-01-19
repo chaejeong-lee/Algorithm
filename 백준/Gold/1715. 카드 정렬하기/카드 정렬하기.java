919ccb267480393a21411e4d295e3be3ae0376e0
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-01-19 11:46:48 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 364 ms, Memory: 25884 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -4,30 +4,25 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		PriorityQueue<Long> pq = new PriorityQueue<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		
 		for(int i=0;i<N;i++) {
-			pq.add(Long.parseLong(br.readLine()));
+			pq.add(Integer.parseInt(br.readLine()));
 		}
 		
-		long num = 0;
+		int num = 0;
 		
-		//들어가있는 개수가 2개 이상이어야 비교 가능함
 		while(pq.size()>1) {
-			long tmp1 = pq.poll();
-			long tmp2 = pq.poll();
+			int tmp1 = pq.poll();
+			int tmp2 = pq.poll();
 			
-			num += tmp1 + tmp2;
-			pq.add(tmp1+tmp2);//마지막 값을 다시 넣어줘야 다시 빼냈을 때 이 합한 값이랑 그 다음 값을 더해주기 때문에
+			num += (tmp1+tmp2);
+			pq.add(tmp1+tmp2);
 		}
 		System.out.println(num);
-		
 	}
-
 }
