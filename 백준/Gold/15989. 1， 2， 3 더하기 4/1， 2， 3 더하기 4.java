551ddc34627c6bad8631ef0682701a1326577832
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-02-19 15:03:44 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 128 ms, Memory: 14704 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		int[][] dp = new int[10001][4];
+		dp[1][1] = 1;
+		dp[2][1] = 1;
+		dp[2][2] = 1;
+		dp[3][1] = 1;
+		dp[3][2] = 1;
+		dp[3][3] = 1;
+		
+		for(int i=4;i<10001;i++) {
+			dp[i][1] = dp[i-1][1];
+			dp[i][2] = dp[i-2][1] + dp[i-2][2];
+			dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+		}
+		
+		for(int tc=0;tc<T;tc++) {
+			int n = Integer.parseInt(br.readLine());
+			sb.append(dp[n][1]+dp[n][2]+dp[n][3]).append("\n");			
+		}
+		System.out.println(sb);
+	}
+
+}
