e3444b141a6099bac07a8383be209b0ff119a950
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-07-27 21:37:51 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.37 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right-left)+1];
+        
+        int idx = 0;
+        for(long i=left; i<=right;i++) {
+            long row = i/n+1;
+            long col = i%n+1;
+            answer[idx++] = (int)Math.max(row, col);
+        }
+        
+        return answer;
+    }
+}
