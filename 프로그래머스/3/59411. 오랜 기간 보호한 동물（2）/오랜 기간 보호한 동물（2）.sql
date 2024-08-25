f2328eee5f2ea4047d0949127698de7ad77f338c
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-08-25 13:15:23 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,8 @@
+-- 입양을 간 동물 중 -> 보호 기간이 가장 길었던 동물 두 마리의 아이디, 이름
+-- 보호 기간이 긴 순으로 조회
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I
+JOIN ANIMAL_OUTS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+ORDER BY DATEDIFF(O.DATETIME, I.DATETIME) DESC
+LIMIT 2;
