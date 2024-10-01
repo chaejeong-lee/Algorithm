12b1d15b0ff8af58077582eceda1ebb22374c4b1
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-10-01 20:56:36 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,19 @@
+SELECT E.EMP_NO
+        , E.EMP_NAME
+        , (CASE
+            WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+            ELSE 'C'
+        END) AS GRADE
+        , (CASE
+            WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+            WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+            WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
+            ELSE 0
+          END) AS BONUS
+FROM HR_EMPLOYEES E
+JOIN HR_GRADE G
+ON E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO
+ORDER BY E.EMP_NO;
