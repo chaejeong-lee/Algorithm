abeabc581bc93e6d4e0c0f17f829a1f9822667d7
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-11-02 23:20:04 +0900

    [level 2] Title: 구명보트, Time: 16.08 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int idx=0;
+        for(int i=people.length-1;i>=idx;i--){
+            if(people[i]+people[idx]<=limit){
+                answer++;
+                idx++;
+            }
+            else answer++;
+        }
+        return answer;
+    }
+}
