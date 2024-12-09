61572285e7eaeda7a7e65c0b3fd11c28a4891229
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-12-09 10:57:41 +0900

    [level 2] Title: 숫자 변환하기, Time: 88.95 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,41 +1,56 @@
 import java.util.*;
+/*
+    문제 풀이 정리
+    - x를 y로 변환하려고 함
+    - 사용할 수 있는 연산
+        1. x에 n을 더함
+        2. x에 2를 곱함
+        3. x에 3을 곱함
+        
+        최소 연산 횟수 return (없으면 -1)
+        
+*/
 
 class Solution {
-    final int NUM = 1000000;
-    boolean[] visited = new boolean[NUM+1];
-    
-    public int solution(int x, int y, int n) {
-        Queue<Integer> q = new LinkedList<>();
-        int cnt = 0;
+    class Number {
+        int num, cnt;
         
-        q.add(x);
-        visited[x] = true;
+        public Number(int num, int cnt) {
+            this.num = num;
+            this.cnt = cnt;
+        }
+    }
+    
+    public int solution(int x, int y, int n) {        
+        boolean[] visited = new boolean[1_000_001];
+        Queue<Number> q = new LinkedList<>();
+        
+        q.add(new Number(x, 0));
         
         while(!q.isEmpty()) {
-            int size = q.size();
+            Number cur = q.poll();
             
-            for(int i=0;i<size;i++) {
-                int cur = q.poll();
-                
-                if(cur == y) return cnt;
-                
-                if(cur+n <= y && !visited[cur+n]) {
-                    visited[cur+n] = true;
-                    q.add(cur+n);
-                }
-                if(cur*2 <= y && !visited[cur*2]) {
-                    visited[cur*2] = true;
-                    q.add(cur*2);
-                }
-                if(cur*3 <= y && !visited[cur*3]) {
-                    visited[cur*3] = true;
-                    q.add(cur*3);
-                }
+            if(cur.num == y) 
+                return cur.cnt;
+            
+            // 1. cur에 n을 더함
+            if(cur.num+n <= y && !visited[cur.num+n]){
+                q.add(new Number(cur.num+n, cur.cnt+1));
+                visited[cur.num+n] = true;
             }
-            cnt++;
+                
+            // 2. cur에 2를 곱함
+            if(cur.num*2 <= y && !visited[cur.num*2]){
+                q.add(new Number(cur.num*2, cur.cnt+1));
+                visited[cur.num*2] = true;
+            }
+            // 3. cur에 3을 곱함
+            if(cur.num*3 <= y && !visited[cur.num*3]){
+                q.add(new Number(cur.num*3, cur.cnt+1));
+                visited[cur.num*3] = true;
+            }
         }
         
         return -1;
     }
-    
 }
