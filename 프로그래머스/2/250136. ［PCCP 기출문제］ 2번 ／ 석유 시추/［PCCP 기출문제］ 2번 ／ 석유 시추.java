1a678b5f379e68e681fe9ae98adb4ea99706e489
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-12-09 21:17:17 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 77.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -4,73 +4,73 @@ class Solution {
     class Point {
         int r, c;
         
-        public Point(int r, int c){
+        public Point(int r, int c) {
             this.r = r;
             this.c = c;
         }
     }
     
-    int n, m;       // n: 세로, m: 가로
-    int[] oilRig;   // oilRig: 시추관 위치별 석유량
+    int n, m;
+    int[] oilAmount;
+    boolean[][] visited;
     
     int[] dr = {-1, 0, 1, 0};
-    int[] dc = {0, -1, 0, 1};
+    int[] dc = {0, 1, 0, -1};
     
     public int solution(int[][] land) {
         n = land.length;
         m = land[0].length;
-        oilRig = new int[m];
-        boolean[][] visited = new boolean[n][m];
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(land[i][j] == 1 && !visited[i][j]){
-                    bfs(land, visited, i, j);
+        oilAmount = new int[m];
+        visited = new boolean[n][m];
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(land[i][j] == 1 && !visited[i][j]) {
+                    bfs(i, j, land);
                 }
             }
         }
         
         int answer = 0;
-        for(int i=0;i<m;i++){
-            answer = answer<oilRig[i]?oilRig[i]:answer;
+        for(int i=0;i<m;i++) {
+            answer = Math.max(answer, oilAmount[i]);
         }
         return answer;
     }
     
-    public void bfs(int[][] land, boolean[][] visited, int r, int c){
-        visited[r][c] = true;
+    public void bfs(int r, int c, int[][] land) {
         Queue<Point> q = new LinkedList<>();
+        
+        visited[r][c] = true;
         q.add(new Point(r, c));
         
-        boolean[] oilRigColumn = new boolean[m];
+        int oilCnt = 1;
+        boolean[] oilColumn = new boolean[m];
         
-        int cnt = 1;
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             Point cur = q.poll();
-            oilRigColumn[cur.c] = true;
+            oilColumn[cur.c] = true;
             
-            for(int d = 0; d<4; d++){
+            for(int d = 0; d<4; d++) {
                 int nr = cur.r + dr[d];
                 int nc = cur.c + dc[d];
                 
                 if(!isRange(nr, nc)) continue;
-                
-                if(land[nr][nc] == 1 && !visited[nr][nc]){
+                if(land[nr][nc] == 1 && !visited[nr][nc]) {
                     q.add(new Point(nr, nc));
                     visited[nr][nc] = true;
-                    cnt+=1;
+                    oilCnt++;
                 }
             }
         }
         
-        for(int i=0;i<m;i++){
-            if(oilRigColumn[i]){
-                oilRig[i] += cnt;
-            }
+        for(int i=0;i<m;i++) {
+            if(oilColumn[i]) oilAmount[i] += oilCnt;
         }
     }
     
-    public boolean isRange(int r, int c){
-        return (0<=r && 0<=c && r<n && c<m);
+    public boolean isRange(int r, int c) {
+        return 0<=r && r<n && 0<= c && c<m;
     }
 }
