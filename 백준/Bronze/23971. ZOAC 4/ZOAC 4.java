e5618b7653836757670deb2f6e2a632bc3c63d50
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-01-16 23:29:42 +0900

    [Bronze III] Title: ZOAC 4, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int H = Integer.valueOf(st.nextToken());
+        int W = Integer.valueOf(st.nextToken());
+        int N = Integer.valueOf(st.nextToken());
+        int M = Integer.valueOf(st.nextToken());
+
+        int height = (H - 1) / (N + 1) + 1;
+        int width = (W - 1) / (M + 1) + 1;
+
+        System.out.print(height * width);
+
+    }
+}
