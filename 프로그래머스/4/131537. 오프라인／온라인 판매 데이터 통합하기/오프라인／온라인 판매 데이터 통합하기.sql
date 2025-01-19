d6294552f411f1cbeb978a070e37f0040a4fe526
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-01-19 17:00:19 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,21 +1,18 @@
--- 2022년 3월의 상품
+-- 2022년 3월의 오프라인/온라인 상품 판매 데이터의 판매 날짜, 상품 id, 유저id, 판매량
 -- offline_sale 테이블의 판매 데이터의 user_id 값은 null로 표시
--- 판매일 기준 오름차순 정렬, 상품id 기준 오름차순, 유저id 기준 오름차순
+-- 판매일 기준으로 오름차순, 상품 id 오름차순, 상품 id 오름차순, 유저 id
+
 (
-    SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") 'SALES_DATE',
-            PRODUCT_ID,
-            USER_ID,
-            SALES_AMOUNT
+    -- 2022년 3월 ONLINE_SALE 테이블 판매 데이터 조회
+    SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
     FROM ONLINE_SALE
-    WHERE MONTH(SALES_DATE) = 3
+    WHERE SALES_DATE LIKE "2022-03%"
     
     UNION
     
-    SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") 'SALES_DATE',
-            PRODUCT_ID,
-            NULL AS USER_ID,
-            SALES_AMOUNT
-    FROM OFFLINE_SALE 
-    WHERE MONTH(SALES_DATE) = 3
+    -- 2022년 3월 OFFLINE_SALE 테이블 판매 데이터 조회
+    SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
+    FROM OFFLINE_SALE
+    WHERE SALES_DATE LIKE "2022-03%"
 )
 ORDER BY SALES_DATE, PRODUCT_ID, USER_ID;
