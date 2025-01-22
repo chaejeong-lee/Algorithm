cffc3677366901ec4d12517e9290dd684e8f8beb
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-01-22 10:40:57 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/README.md
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 중성화 여부 파악하기 - 59409 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59409?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59409) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 09월 26일 21:20:02
+2025년 01월 22일 10:40:54
 
 ### 문제 설명
 
--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,13 @@
-SELECT 
-    ANIMAL_ID, 
-    NAME, 
-    CASE 
-        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
-        ELSE 'X'
-    END AS '중성화'
+-- 보호소의 동물이 중성화되었는지 아닌지 판단
+-- 중성화된 동물은 SEX_UPON_INTAKE 컬럼에 'Neutered' 또는 'Spayed'가 들어있음
+-- 아이디, 이름, 중성화 여부
+-- 아이디 순으로 조회
+-- 중성화 되어있다면 'O' 아니라면 'X'
+SELECT ANIMAL_ID, 
+        NAME, 
+        CASE 
+            WHEN SEX_UPON_INTAKE LIKE 'Neutered%' OR SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O' 
+            ELSE 'X'
+        END AS '중성화'
 FROM ANIMAL_INS
-ORDER BY ANIMAL_ID ASC;
+ORDER BY ANIMAL_ID;
