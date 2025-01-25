31a8d692d7bec2fae63d9e52d87ff34ab87c94ff
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-01-25 23:25:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 66.45 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = -1;
+        char[] carr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();   
+    
+        for (int i=0;i<carr.length;i++) {
+            char c = carr[i];
+            if (stack.isEmpty()) stack.push(c);
+            else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }        
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
