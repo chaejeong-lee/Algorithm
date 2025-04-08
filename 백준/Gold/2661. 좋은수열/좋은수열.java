2198de10ae72ce679dea10b679cd49a9a2526e14
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-04-08 22:45:36 +0900

    [Gold IV] Title: 좋은수열, Time: 132 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -1,39 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N;
+	
+	static boolean check = false;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-
-        backtracking("");
+		int N = Integer.parseInt(br.readLine());
+		findGoodSequence("", N);
 	}
-
-    private static void backtracking(String result) {
-        if (result.length() == N) {
-            System.out.println(result);
-            System.exit(0);
-        } else {
-            for (int i = 1; i <= 3; i++) {
-                if (isGoodSequence(result + i)) {
-                    backtracking(result + i);
-                }
-            }
-        }
-    }
-    
-    private static boolean isGoodSequence(String s) {
-        int length = s.length() / 2;
-
-        for (int i = 1; i <= length; i++) {
-            if (s.substring(s.length() - i).equals(s.substring(s.length() - 2 * i, s.length() - i))) {
+	
+	private static void findGoodSequence(String str, int n) {
+		if(check) return;
+		else if(str.length() == n) {
+			System.out.println(str);
+			check = true;
+			return;
+		}
+		else {
+			for(int i=1;i<=3;i++) {
+				if(checkGoodSequence(str+i)) {
+					findGoodSequence(str+i, n);
+				}
+			}
+		}
+	}
+	
+	private static boolean checkGoodSequence(String str) {
+		int len = str.length() / 2;
+        
+		for(int i=1;i<=len;i++) {
+			if (str.substring(str.length() - i).equals(str.substring(str.length() - 2 * i, str.length() - i))) {
                 return false;
             }
-        }
-        return true;
-    }
+		}
+		
+		return true;
+	}
+
 }
