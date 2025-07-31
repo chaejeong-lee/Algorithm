728dacb6c32eafd88daaf5568ae4541e5646e467
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2025-07-31 19:09:51 +0900

    [Gold IV] Title: 미친 로봇, Time: 168 ms, Memory: 15160 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.java
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.java
@@ -1,47 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	static int n;
-	static int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};//동 서 남 북
+	static int N;
 	static double[] percent;
 	static boolean[][] visited;
 	static double answer=0;
+	
+	static int[] dr = {0, 0, 1, -1};
+	static int[] dc = {1, -1, 0, 0};
 
 	public static void main(String[] args) throws IOException {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		 n = Integer.parseInt(st.nextToken());
-		 
-		 percent = new double[4];
-		 for(int i=0;i<4;i++) {//동 서 남 북
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		
+		percent = new double[4];
+		for(int i=0;i<4;i++) {//동 서 남 북
 			 percent[i] = Integer.parseInt(st.nextToken())*0.01;
-		 }
-		 
-		 visited = new boolean[30][30];
-		 dfs(15, 15, 0, 1);
-		 System.out.println(answer);
+		}
+		
+		visited = new boolean[30][30];
+		dfs(15, 15, 0, 1);
+		System.out.println(answer);
 	}
-
-	private static void dfs(int x, int y, int idx, double total) {
-		if(idx == n) {
+	
+	private static void dfs(int r, int c, int idx, double total) {
+		if(idx == N) {
 			answer += total;
 			return;
 		}
 		
-		visited[x][y] = true;
+		visited[r][c] = true;
 		for(int i=0;i<4;i++) {
-			int nx = x + dir[i][0];
-			int ny = y + dir[i][1];
+			int nr = r + dr[i];
+			int nc = c + dc[i];
 			
-			if(nx>=0 && ny>=0 && nx<30 && ny<30) {
-				if(!visited[nx][ny]) {
-					visited[nx][ny] = true;
-					dfs(nx, ny, idx+1, total*percent[i]);
-					visited[nx][ny] = false;
+			if(nr>=0 && nc>=0 && nr<30 && nc<30) {
+				if(!visited[nr][nc]) {
+					visited[nr][nc] = true;
+					dfs(nr, nc, idx+1, total*percent[i]);
+					visited[nr][nc] = false;
 				}
 			}
 		}
