db928eac98cdfda4bbfec8b08d0bdb39d0ad6ade
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2026-02-23 15:00:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,9 +3,22 @@ import java.util.*;
 
 public class Main {
 	
-	static int N, M, cnt = 1;
-	static int[][] map;
+	static class Point {
+		int r, c, d;
+		
+		public Point(int r, int c, int d) {
+			this.r = r;
+			this.c = c;
+			this.d = d;
+		}
+	}
 	
+	static int N, M;
+	static Point robot;
+	static int[][] map;
+	static int answer = 0;
+	
+	// 북 -> 동 -> 남 -> 서
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
 
@@ -15,11 +28,11 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		// 로봇 청소기 좌표
 		st = new StringTokenizer(br.readLine());
-		int robotR = Integer.parseInt(st.nextToken());
-		int robotC = Integer.parseInt(st.nextToken());
-		int robotDir = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		robot = new Point(r, c, d);
 		
 		map = new int[N][M];
 		for(int i=0;i<N;i++) {
@@ -28,39 +41,58 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		// 입력 끝
 		
-		cleanMap(robotR, robotC, robotDir);
-		System.out.println(cnt);
+		startToClean();
+		System.out.println(answer);
 	}
-	
-	public static void cleanMap(int r, int c, int dir) {
-		map[r][c] = -1;
+
+	public static void startToClean() {
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(robot.r, robot.c, robot.d));
 		
-		for(int d = 0; d<4; d++) {
-			// 반시계 방향
-			dir = (dir + 3)%4;
-			int nr = r + dr[dir];
-			int nc = c + dc[dir];
+		while(!q.isEmpty()) {
+			Point cur = q.poll();
 			
-			if(!isRange(nr, nc)) continue;
-			if(map[nr][nc] == 0) {
-				cnt++;
-				cleanMap(nr, nc, dir);
-				return;
+			if(map[cur.r][cur.c] == 0) {
+	            map[cur.r][cur.c] = 2;
+	            answer++;
+	        }
+			
+			// 청소되지 않은 빈칸이 없는 경우
+			boolean foundEmpty = false;
+			int nextD = cur.d;
+			
+			for(int d = 0; d<4; d++) {
+				nextD = (nextD + 3) % 4;
+				int nr = cur.r + dr[nextD];
+				int nc = cur.c + dc[nextD];
+				
+				// 범위를 들어오는지 && 청소를 안 했는지
+				if(isRange(nr, nc) && map[nr][nc] == 0) {
+					foundEmpty = true;
+					q.add(new Point(nr, nc, nextD));
+					break;
+				}
 			}
-		}
-		
-		// 후진
-		// 바라보는 방향은 유지해야함.
-		int nDir = (dir + 2)%4;
-		int nr = r + dr[nDir];
-		int nc = c + dc[nDir];
-		if(isRange(nr, nc) && map[nr][nc] != 1) {
-			cleanMap(nr, nc, dir);
+			
+			if(!foundEmpty) {
+				// 청소할 칸이 없는 경우 => 후진
+				int nr = cur.r + dr[(cur.d + 2)%4];
+				int nc = cur.c + dc[(cur.d + 2)%4];
+				
+				// 뒤로 갈 수 없는 경우
+				if(isRange(nr, nc) && map[nr][nc] != 1) {
+					q.add(new Point(nr, nc, cur.d));
+				}
+				else {
+					return;
+				}
+			}
 		}
 	}
 	
 	public static boolean isRange(int r, int c) {
-		return 0<=r && r<N && 0<=c && c<M;
+		return (0<= r && r < N && 0<=c && c<M);
 	}
 }
