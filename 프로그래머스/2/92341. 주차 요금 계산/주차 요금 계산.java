9e7da66114be7729775392d4e9bdb8a9fedaec48
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2026-06-25 21:41:58 +0900

    [level 2] Title: 주차 요금 계산, Time: 18.16 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,36 +1,82 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    public int[] solution(int[] fees, String[] records) {
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0;i<records.length;i++) {
-            StringTokenizer st = new StringTokenizer(records[i]);
-            String strTime = st.nextToken();
-            String[] tmp = strTime.split(":");
-            int time = Integer.parseInt(tmp[0])*60 + Integer.parseInt(tmp[1]);
-            String car = st.nextToken();
-            String inOut = st.nextToken();
-            
-            map.put(car, map.getOrDefault(car, 0) + (inOut.equals("IN")? - time:time));
+    class Time {
+        int in_time, out_time;
+        
+        public Time(int in_time, int out_time) {
+            this.in_time = in_time;
+            this.out_time = out_time;
+        }
+    }
+    
+    class Money implements Comparable<Money> {
+        String num;
+        int fee;
+        
+        public Money(String num, int fee) {
+            this.num = num;
+            this.fee = fee;
         }
         
-        ArrayList<String> keyList = new ArrayList<>(map.keySet());
-        Collections.sort(keyList);
+        @Override
+        public int compareTo(Money m) {
+            return this.num.compareTo(m.num);
+        }
+    }
+    
+    public int[] solution(int[] fees, String[] records) {
+        StringTokenizer st = null;
         
-        int[] answer = new int[map.size()];
-        for(int i=0;i<keyList.size();i++) {
-            answer[i] = map.get(keyList.get(i));
-            if(answer[i] <= 0) {
-                answer[i] += 23 * 60 + 59;
+        HashMap<String, Time> hm = new HashMap<>();
+        PriorityQueue<Money> pq = new PriorityQueue<>();
+        HashMap<String, Integer> mm = new HashMap<>();
+        
+        for(int i=0;i<records.length;i++) {
+            st = new StringTokenizer(records[i], " ");
+            
+            String time = st.nextToken();
+            String num = st.nextToken();
+            String status = st.nextToken();
+            
+            st = new StringTokenizer(time, ":");
+            int timeTotal = Integer.parseInt(st.nextToken())*60 + Integer.parseInt(st.nextToken());
+            
+            if(status.equals("IN")) {
+                hm.put(num, new Time(timeTotal, 23*60 + 59));
+            }
+            else {
+                hm.put(num, new Time(hm.get(num).in_time, timeTotal));
+                int t = timeTotal - hm.get(num).in_time;
+                mm.put(num, mm.containsKey(num)? mm.get(num) + t: t);
+                hm.remove(num);
             }
             
-            answer[i] -= fees[0];
+        }
+        for(Map.Entry<String, Time> e : hm.entrySet()) {
+            String num = e.getKey();
+            Time time = e.getValue();
             
-            int fee = fees[1];
-            if(answer[i] > 0) {
-                fee += Math.ceil((double)answer[i] / fees[2]) * fees[3];
-            }
-            answer[i] = fee;
+            int t = time.out_time - time.in_time;
+            mm.put(num, mm.containsKey(num)? mm.get(num) + t: t);
+        }
+        
+        for(Map.Entry<String, Integer> e : mm.entrySet()) {
+            String num = e.getKey();
+            int time = e.getValue();
+            
+            int totalTime = time - fees[0];
+            System.out.println(time + " : " +totalTime);
+            int money = totalTime < 0? fees[1]: (fees[1] + (int)Math.ceil(totalTime*1.0 / fees[2])*fees[3]);
+            pq.add(new Money(num, money));
+        }
+        
+        int[] answer = new int[pq.size()];
+        int a = 0;
+        while(!pq.isEmpty()) {
+            Money cur = pq.poll();            
+            answer[a++] = cur.fee;
         }
         return answer;
     }
